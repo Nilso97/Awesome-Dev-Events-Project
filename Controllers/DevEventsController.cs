8309010c10e1b3464ff853a73fb6025da3bcd4ed
Nilso97 <nilsojunior90@gmail.com>
2023-03-29 07:14:29 -0300

    adicionando documentação com Swagger

--- Controllers/DevEventsController.cs
+++ Controllers/DevEventsController.cs
@@ -16,7 +16,13 @@ namespace Dev_Events_App.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// Obter todos os eventos
+        /// </summary>
+        /// <returns>Coleção de eventos</returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetAllDevEvents()
         {
             var devEvents = _context.DevEvents.Where(
@@ -26,7 +32,16 @@ namespace Dev_Events_App.Controllers
             return Ok(devEvents);
         }
 
+        /// <summary>
+        /// Obter um os eventos
+        /// </summary>
+        /// <param name="id">Identificador do evento</param>
+        /// <returns>Dados do evento</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetEventById(Guid id)
         {
             var devEvent = _context.DevEvents
@@ -40,7 +55,34 @@ namespace Dev_Events_App.Controllers
             return NotFound();
         }
 
+        /// <summary>
+        /// Cadastrar um evento
+        /// </summary>
+        /// <remarks>
+        /// {
+        ///   "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        ///   "title": "string",
+        ///   "description": "string",
+        ///   "startDate": "2023-03-29T10:03:40.674Z",
+        ///   "endDate": "2023-03-29T10:03:40.674Z",
+        ///   "isDeleted": true,
+        ///   "speakers": [
+        ///     {
+        ///       "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        ///       "name": "string",
+        ///       "talkTitle": "string",
+        ///       "talkDescription": "string",
+        ///       "linkedInProfile": "string",
+        ///       "devEventId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        ///     }
+        ///   ]
+        /// }
+        /// </remarks>
+        /// <param name="devEvent">Dados do evento</param>
+        /// <returns>Objeto recém-criado</returns>
+        /// <response code="201">Criado com sucesso</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public IActionResult SaveEvent([FromBody] DevEvent devEvent)
         {
             _context.DevEvents.Add(devEvent);
@@ -53,7 +95,37 @@ namespace Dev_Events_App.Controllers
             );
         }
 
+        /// <summary>
+        /// Atualizar um evento
+        /// </summary>
+        /// <remarks>
+        /// {
+        ///   "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        ///   "title": "string",
+        ///   "description": "string",
+        ///   "startDate": "2023-03-29T10:03:40.674Z",
+        ///   "endDate": "2023-03-29T10:03:40.674Z",
+        ///   "isDeleted": true,
+        ///   "speakers": [
+        ///     {
+        ///       "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        ///       "name": "string",
+        ///       "talkTitle": "string",
+        ///       "talkDescription": "string",
+        ///       "linkedInProfile": "string",
+        ///       "devEventId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        ///     }
+        ///   ]
+        /// }
+        /// </remarks>
+        /// <param name="id">Identificador do evento</param>
+        /// <param name="input">Dados do evento</param>
+        /// <returns>Não retorna nada</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult UpdateEvent(Guid id, [FromBody] DevEvent input)
         {
             var devEvent = _context.DevEvents.SingleOrDefault(
@@ -78,7 +150,16 @@ namespace Dev_Events_App.Controllers
             return NotFound();
         }
 
+        /// <summary>
+        /// Deletar um evento
+        /// </summary>
+        /// <param name="id">Identificador do evento</param>
+        /// <returns>Não retorna nada</returns>
+        /// <response code="204">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult DeleteEvent(Guid id)
         {
             var devEvent = _context.DevEvents.SingleOrDefault(
@@ -97,7 +178,27 @@ namespace Dev_Events_App.Controllers
             return NotFound();
         }
 
+        /// <summary>
+        /// Adicionar um palestrante à um evento
+        /// </summary>
+        /// <remarks>
+        ///         {
+        ///   "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        ///   "name": "string",
+        ///   "talkTitle": "string",
+        ///   "talkDescription": "string",
+        ///   "linkedInProfile": "string",
+        ///   "devEventId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        /// }
+        /// </remarks>
+        /// <param name="id">Identificador do evento</param>
+        /// <param name="speaker">Dados do palestrante</param>
+        /// <returns>Não retorna nada</returns>
+        /// <response code="204">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpPost("{id}/palestrante")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult SaveSpeakerInEvent(Guid id, DevEventSpeaker speaker)
         {
             speaker.DevEventId = id;
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Dev_Events_App.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace Dev_Events_App
 {
@@ -20,7 +22,25 @@ namespace Dev_Events_App
             builder.Services.AddControllers();
 
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "DevEventsApp.API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nilso Junior",
+                        Email = "nilsojunior90@gmail.com",
+                        Url = new Uri("https://github.com/Nilso97")
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
 
             var app = builder.Build();
 
